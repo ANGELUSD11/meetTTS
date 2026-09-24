df8fbc388f4a24f923d52fc9bdb8894004739104
ANGELUSD11 <angelleonardohern3@gmail.com>
2026-09-23 23:59:37 -0500

    Fix Bookmarklet URL parser

---
+++ check_csp.py
@@ -0,0 +1,7 @@
+﻿import urllib.request
+
+try:
+    response = urllib.request.urlopen('https://meet.google.com/')
+    print(response.headers)
+except Exception as e:
+    print(e.headers)
---
+++ check_csp2.py
@@ -0,0 +1,7 @@
+﻿import urllib.request
+
+try:
+    response = urllib.request.urlopen('https://meet.google.com/kxq-woqb-jwu')
+    print(response.headers)
+except Exception as e:
+    print(e.headers)
